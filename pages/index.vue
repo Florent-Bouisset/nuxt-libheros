3b9aae3a9c5aa32f9de2c081c1c2ce9ee38d8bd9
Florent Bouisset <florent.bouisset@jellysmack.com>
2023-03-06 12:20:13 +0100

    add illustration

--- pages/index.vue
+++ pages/index.vue
@@ -8,25 +8,30 @@ useHead({
   <div class="">
     <article class="lg:px-16">
       <p><strong>Bienvenue sur Libhéros,</strong></p>
-      <p>
-        Votre plateforme e-santé dédiée à la recherche de médicaments en stock
-        dans les pharmacies proches de chez vous.
-      </p>
-      <p>
-        Avec Libhéros, vous pouvez facilement trouver les médicaments dont vous
-        avez besoin sans avoir à parcourir de nombreuses pharmacies.
-      </p>
-      <p>
-        Notre service de localisation de pharmacie vous permet de trouver
-        rapidement une pharmacie qui a le médicament que vous cherchez, ainsi
-        que toutes les informations sur la disponibilité et les prix.
-      </p>
-      <p>
-        Avec notre système de recherche de médicaments vous pouvez être sûr de
-        trouver les médicaments dont vous avez besoin en quelques clics
-        seulement. N'hésitez pas à utiliser notre service pour une expérience de
-        recherche de médicaments facile et pratique.
-      </p>
+      <div class="flex">
+        <div>
+          <p>
+            Votre plateforme e-santé dédiée à la recherche de médicaments en
+            stock dans les pharmacies proches de chez vous.
+          </p>
+          <p>
+            Avec Libhéros, vous pouvez facilement trouver les médicaments dont
+            vous avez besoin sans avoir à parcourir de nombreuses pharmacies.
+          </p>
+          <p>
+            Notre service de localisation de pharmacie vous permet de trouver
+            rapidement une pharmacie qui a le médicament que vous cherchez,
+            ainsi que toutes les informations sur la disponibilité et les prix.
+          </p>
+          <p>
+            Avec notre système de recherche de médicaments vous pouvez être sûr
+            de trouver les médicaments dont vous avez besoin en quelques clics
+            seulement. N'hésitez pas à utiliser notre service pour une
+            expérience de recherche de médicaments facile et pratique.
+          </p>
+        </div>
+        <img src="/pills.png" class="object-contain hidden sm:block" />
+      </div>
     </article>
   </div>
 </template>
